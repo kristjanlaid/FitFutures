0d3f00800cbbcd52c9649020a03aa8bb6c3aed1e
Aleksanderkampus <aleksander.kampus@gmail.com>
2023-11-11 01:28:30 +0200

    Added navbar and main page navigation

---
+++ fitfutures/lib/screens/main_screen.dart
@@ -0,0 +1,41 @@
+import 'package:fitfutures/widgets/bottom_nav_bar.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  final _screens = const <Widget>[
+    Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [Text("Test")],
+    ),
+    Column(
+      children: [Text("Test")],
+    ),
+    Column(
+      children: [Text("Test")],
+    )
+  ];
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        bottomNavigationBar: BottomNavBar(
+            selectedIndex: _selectedIndex, onItemTapped: _onItemTapped),
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: _screens,
+        ));
+  }
+}
